03f1b603bf031d25aaaddc0ada82c25aad8e719e
mayasahu <akratisahu1234@gmail.com>
2023-04-07 22:24:00 +0530

    commit message

---
+++ src/test/java/Practice/TestTest.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class TestTest {
+
+	@Test
+	public void Method6() {
+		System.out.println("----Test01------");
+	}
+	@Test
+ public void Method5() {
+	System.out.println("----Test02------");	
+	}
+}
